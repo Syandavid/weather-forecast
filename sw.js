f8930148c36c04fa0fef1f87b52d2561e3d364cf
syan1209david-droid <syan1209david@gmail.com>
2026-08-30 21:56:30 +0800

    Refresh service worker cache for updated app

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "weather-forecast-v8";
+const CACHE = "weather-forecast-v9";
 const ASSETS = [
   "./",
   "./index.html",
@@ -83,3 +83,4 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
